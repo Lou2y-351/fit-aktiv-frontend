9729b7cc813ad509bcacb1429506a43ab714694d
Lou2y-351 <Louaysaleh351@gmail.com>
2026-06-24 17:10:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react"
-
+import { useState, useEffect } from "react"
+import { api } from "./api"
 // ══════════════════════════════════════════════════════════════════════════════
 // DATE / WEEK HELPERS
 // ══════════════════════════════════════════════════════════════════════════════
